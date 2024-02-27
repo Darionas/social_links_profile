a29a71eda02089035d37414fd4ccf36acd36ed9c
Dario <darionas@gmail.com>
2024-02-27 22:23:20 +0200

    update_3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'social_links_profile';
+  title = 'Social links profile';
 }
--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrl: './main.component.css'
 })
 export class MainComponent {
-
+    person = [
+      {
+        fullName: 'Jessica Randall',
+        city: 'Klaipeda',
+        country: 'Lithuania',
+        photo: 'assets/images/avatar-jessica.jpeg',
+        links: {
+          GitHub: 'https://github.com/Darionas',
+          Frontend_Mentor: 'https://www.frontendmentor.io/profile/Darionas',
+          Linkedin: 'https://www.linkedin.com/in/dario-dario-2a3118231/'
+        }
+      }
+    ];
 }

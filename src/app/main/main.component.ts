cb86af657b8c7656f210bbe71d3cd690b366b449
Dario <darionas@gmail.com>
2024-02-28 21:27:27 +0200

    update_3

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -5,18 +5,21 @@ import { Component } from '@angular/core';
   templateUrl: './main.component.html',
   styleUrl: './main.component.css'
 })
+
 export class MainComponent {
     person = [
       {
         fullName: 'Jessica Randall',
         city: 'Klaipeda',
         country: 'Lithuania',
-        photo: 'assets/images/avatar-jessica.jpeg',
-        links: {
-          GitHub: 'https://github.com/Darionas',
-          Frontend_Mentor: 'https://www.frontendmentor.io/profile/Darionas',
-          Linkedin: 'https://www.linkedin.com/in/dario-dario-2a3118231/'
-        }
+        photo: 'assets/images/avatar-jessica.jpeg'
       }
-    ];
+    ]; 
+    
+    links = {
+        GitHub: 'https://github.com/Darionas',
+        Frontend_Mentor: 'https://www.frontendmentor.io/profile/Darionas',
+        Linkedin: 'https://www.linkedin.com/in/dario-dario-2a3118231/'
+    };
+  
 }
